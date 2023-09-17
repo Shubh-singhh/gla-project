ad4dfe6e4fc22e665c5b273c4f4b43fbc9b3a6f4
CodingPrakharKapoor <prakharkapoorcollege@gmail.com>
2023-09-17 21:44:45 +0530

    added working login page with authentication email

---
+++ Login_Page/prakhar_login/info.py
@@ -0,0 +1,10 @@
+'''
+to use this feature email should have 2-step verification and in your main 
+account go to app password>> generate password and copy that password here it will work
+'''
+
+EMAIL_USE_TLS =True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'demo.prakhar@gmail.com'
+EMAIL_HOST_PASSWORD ='qmlrsvcwbrnfbagw'
+EMAIL_PORT = 587
