e7a34f35a9f2a395dac8ba79f3eed404fef1cfcc
Prakhar Kapoor <113131584+CodingPrakharKapoor@users.noreply.github.com>
2023-09-20 06:36:12 +0530

    changed sender email from demo.prakhar to swipe2clean.ecoexperts

--- Login_Page/prakhar_login/info.py
+++ Login_Page/prakhar_login/info.py
@@ -5,6 +5,6 @@ account go to app password>> generate password and copy that password here it wi
 
 EMAIL_USE_TLS =True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'demo.prakhar@gmail.com'
-EMAIL_HOST_PASSWORD ='qmlrsvcwbrnfbagw'
+EMAIL_HOST_USER = 'swipe2clean.ecoexperts@gmail.com'
+EMAIL_HOST_PASSWORD ='joreyzaujlblwljg'
 EMAIL_PORT = 587
